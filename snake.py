e7b088a58e71c613f5afbecf3f09ad98bf136fba
Stephen Nancekivell <stephennancekivell@gmail.com>
2011-10-09 11:10:24 +1300

    snake pit

--- snake.py
+++ snake.py
@@ -3,15 +3,18 @@
 import time
 import random
 
-class snake:
+class Snake:
     size = 0
     pos = [] # list of (row,column) head is at position 0
 
-    weight_target =2
-    weight_selfd = -1
+    def __str__(self):
+        return "<%s> %s wt:%i ws%i" %(hex(id(self)),self.pos[0], self.weight_target,self.weight_selfd)
 
     def __init__(self,game):
         self.game=game
+        self.pos = [(1,1),(1,2)]#.append((0,0))
+        self.weight_target = random.uniform(0,5)
+        self.weight_selfd = random.uniform(-5,0)
 
     def available_moves(self):
         # all moves from head than are not in pos
@@ -43,6 +46,16 @@ class snake:
         w = self.weight_target* self.game.score_X(move)
         return w + (self.weight_selfd*self.distance_score(move))
 
+    def tick(self):
+        m = self.best_move()
+        if m ==-1: return 'lose'
+        elif m == self.game.target:
+            self.grow_to(m)
+            return 'target'
+        else:
+            self.move(m)
+            return ''
+
     def best_move(self):
         # returns -1 when no move, must have died, lost game.
         am = self.available_moves()
@@ -61,9 +74,12 @@ class snake:
             s += snake_game.distance_between(pos,cell)
         return s
 
-    @classmethod
-    def mate(game,s1,s2):
-        child = snake(game)
+    @staticmethod
+    def mate(s1,s2):
+        if s1.game != s2.game:
+            raise "Error mating snakes from different games"
+        child = Snake(s1.game)
+        child.pos = s1.pos # should i check the pos is the same?
         child.weight_target = float(s1.weight_target + s2.weight_target)/2
         child.weight_selfd = float(s1.weight_selfd + s2.weight_selfd)/2
         return child
@@ -73,7 +89,7 @@ class snake_game:
     target = (5,5)
 
     def __init__(self):
-        self.snake = snake(self)
+        self.snake = Snake(self)
 
     def draw_grid(self):
         print '----------'
@@ -95,24 +111,16 @@ class snake_game:
         return snake_game.distance_between(cell, self.target)
 
     def new_target(self):
+        # this will miss lots when the number of free cells is small
         while 1:
             t = (random.randint(0,self.grid_size[0]),
-                random.randint(0,self.grid_size[0]))
+                random.randint(0,self.grid_size[1]))
             if t not in self.snake.pos:
                 return t
 
     def tick(self):
-        # run one time_step
-        m = self.snake.best_move()
-        if m ==-1: return 'lose'
-        if m != self.target:
-            self.snake.move(m)
-        else:
-            self.snake.grow_to(m)
-            return 'target'
-
-        return ''
-
+        return self.snake.tick()
+       
     def play(self):
         while 1:
             c = self.tick()
@@ -123,9 +131,83 @@ class snake_game:
             if c == 'lose': break
             time.sleep(0.3)
 
+    def play_evolving(self,population_size):
+        snakes = {self.snake: 0}
+        for i in range(population_size):
+            print Snake(self)
+            snakes[Snake(self)] =0
+
+        while 'playing game':
+            for snake in snakes:
+                play_count=0
+                while play_count < 30:
+                    play_count+=1
+                    res = snake.tick()
+                    if res == 'target':
+                        snakes[snake]=play_count
+                        break
+                    elif res =='lose':
+                        snakes[snake] = -1
+                        break
+
+            print snakes
+            break
+            #snakes = [mate snakes in tornament]
+
+class e_pit:
+    # the snake pit full of evolving snakes
+    def __init__(self,size):
+        self.game = snake_game()
+        self.snakes = []
+        for i in range(size):
+            self.snakes.append(Snake(self.game))
+
+    def tick(self):
+        times = {}
+
+        for snake in self.snakes:
+            self.game.snake = snake
+            print snake,snake.pos
+            tick_count=0
+            while tick_count <= 40:
+                self.game.draw_grid()
+                time.sleep(0.2)
+                tick_count+=1
+                res = snake.tick()
+                if res == 'target':
+                    times[snake]=tick_count
+                    break
+                elif res =='lose':
+                    times[snake] = -1
+                    break
+        print times
+
+        if len(times)==0:
+            raise Exception('all snakes went extinct, nothing to evolve')
+
+        # mating tornament
+        snakes2=[]
+        for i in range(len(self.snakes)):
+            m1 = random.choice(times.keys())
+            m2 = random.choice(times.keys())
+            f1 = random.choice(times.keys())
+            f2 = random.choice(times.keys())
+
+            f = f1
+            if times[f2] < times[f1]: f = f2
+            m = m1
+            if times[m2] < times[m1]: m = m2
+    
+            snakes2.append(Snake.mate(f,m))
+        self.snakes = snakes2
 
 if __name__=='__main__':
-    sg= snake_game()
-    sg.snake.pos.append((1,1))
-    sg.snake.pos.append((1,2))
-    sg.play()
+    #sg= snake_game()
+    #sg.snake.pos.append((1,1))
+    #sg.snake.pos.append((1,2))
+    e = e_pit(3)
+    e.tick()
+    e.game.target = e.game.new_target()
+    e.tick()
+
+
